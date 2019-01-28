f4d240e7a602e82400ff5b4ef3588f30973e8779
snoopysecurity <sanoop_sam@hotmail.com>
2019-01-28 14:00:15 +0000

    Create Insomnihack_2019_l33t-hoster.php

---
+++ Insecure File Uploads/Insomnihack_2019_l33t-hoster.php
@@ -0,0 +1,82 @@
+<?php
+if (isset($_GET["source"]))
+    die(highlight_file(__FILE__));
+
+session_start();
+
+if (!isset($_SESSION["home"])) {
+    $_SESSION["home"] = bin2hex(random_bytes(20));
+}
+$userdir = "images/{$_SESSION["home"]}/";
+if (!file_exists($userdir)) {
+    mkdir($userdir);
+}
+
+$disallowed_ext = array(
+    "php",
+    "php3",
+    "php4",
+    "php5",
+    "php7",
+    "pht",
+    "phtm",
+    "phtml",
+    "phar",
+    "phps",
+);
+
+
+if (isset($_POST["upload"])) {
+    if ($_FILES['image']['error'] !== UPLOAD_ERR_OK) {
+        die("yuuuge fail");
+    }
+
+    $tmp_name = $_FILES["image"]["tmp_name"];
+    $name = $_FILES["image"]["name"];
+    $parts = explode(".", $name);
+    $ext = array_pop($parts);
+
+    if (empty($parts[0])) {
+        array_shift($parts);
+    }
+
+    if (count($parts) === 0) {
+        die("lol filename is empty");
+    }
+
+    if (in_array($ext, $disallowed_ext, TRUE)) {
+        die("lol nice try, but im not stupid dude...");
+    }
+
+    $image = file_get_contents($tmp_name);
+    if (mb_strpos($image, "<?") !== FALSE) {
+        die("why would you need php in a pic.....");
+    }
+
+    if (!exif_imagetype($tmp_name)) {
+        die("not an image.");
+    }
+
+    $image_size = getimagesize($tmp_name);
+    if ($image_size[0] !== 1337 || $image_size[1] !== 1337) {
+        die("lol noob, your pic is not l33t enough");
+    }
+
+    $name = implode(".", $parts);
+    move_uploaded_file($tmp_name, $userdir . $name . "." . $ext);
+}
+
+echo "<h3>Your <a href=$userdir>files</a>:</h3><ul>";
+foreach(glob($userdir . "*") as $file) {
+    echo "<li><a href='$file'>$file</a></li>";
+}
+echo "</ul>";
+
+?>
+
+<h1>Upload your pics!</h1>
+<form method="POST" action="?" enctype="multipart/form-data">
+    <input type="file" name="image">
+    <input type="submit" name=upload>
+</form>
+<!-- /?source -->
