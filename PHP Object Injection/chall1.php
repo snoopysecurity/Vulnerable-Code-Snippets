3d1e4e514c0bc21bc28110f25cdb312df2764d9b
Sam Sanoop <sanoop_sam@hotmail.com>
2020-12-30 14:02:37 +0000

    Create chall1.php

---
+++ PHP Object Injection/chall1.php
@@ -0,0 +1,21 @@
+class Example1
+{
+   public $cache_file;
+
+   function __construct()
+   {
+      // some PHP code...
+   }
+
+   function __destruct()
+   {
+      $file = "/var/www/cache/tmp/{$this->cache_file}";
+      if (file_exists($file)) @unlink($file);
+   }
+}
+
+// some PHP code...
+
+$user_data = unserialize($_GET['data']);
+
+// some PHP code...
