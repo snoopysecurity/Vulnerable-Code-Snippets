14500daff3acec7b33c4b92e49ba12f59194a950
Sam Sanoop <sanoop_sam@hotmail.com>
2021-10-26 15:54:21 +0100

    Update graphql.js

--- Sensitive Data Exposure/graphql.js
+++ Sensitive Data Exposure/graphql.js
@@ -6,7 +6,7 @@ const server = new ApolloServer({
     typeDefs,
     resolvers,
     playground: true,
-    validationRules: [ depthLimit(10) ],
+    validationRules: [depthLimit(10)],
     introspection: true,
     context: ({ req, res }) => ({
       headers: req.headers,
