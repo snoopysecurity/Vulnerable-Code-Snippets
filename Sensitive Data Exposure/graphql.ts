ff5611f30eeebad2e579dc6a83b7f49f37f7c15b
Sam Sanoop <sanoop_sam@hotmail.com>
2021-10-26 16:01:55 +0100

    Update graphql.ts

--- Sensitive Data Exposure/graphql.ts
+++ Sensitive Data Exposure/graphql.ts
@@ -4,9 +4,9 @@ import graphqlHTTP from 'express-graphql'
 import schema from './schema'
 
 
-const app = express() //dcexpect DisablePoweredBy
+const app = express() 
 // depthlimit prevents nested queries
-app.use('/graphql', graphqlHTTP((req, res) => ({ //dcexpect IntrospectionEnabled
+app.use('/graphql', graphqlHTTP((req, res) => ({ 
   schema,
   validationRules: [ depthLimit(10) ]
 })))
