33cbc9dd6c1c46a202c136564d4f111049d72e5f
Sam Sanoop <sams@snyk.io>
2022-01-16 21:49:54 +0000

    Create WishList.php
    The challenge contains an arbitrary file upload vulnerability in line 13. The operation in_array() is used in line 12 to check if the file name is a number. However, it is type-unsafe because the third parameter is not set to 'true'. Hence, PHP will try to type-cast the file name to an integer value when comparing it to the array $whitelist (line 8). As a result it is possible to bypass the whitelist by prepending a value in the range of 1 and 24 to the file name, for example "5backdoor.php". The uploaded PHP file then leads to code execution on the web server.

---
+++ Insecure File Uploads/WishList.php
@@ -0,0 +1,21 @@
+class Challenge {
+    const UPLOAD_DIRECTORY = './solutions/';
+    private $file;
+    private $whitelist;
+
+    public function __construct($file) {
+        $this->file = $file;
+        $this->whitelist = range(1, 24);
+    }
+
+    public function __destruct() {
+        if (in_array($this->file['name'], $this->whitelist)) {
+            move_uploaded_file(
+                $this->file['tmp_name'],
+                self::UPLOAD_DIRECTORY . $this->file['name']
+            );
+        }
+    }
+}
+
+$challenge = new Challenge($_FILES['solution']);
