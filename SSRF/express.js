185897d7915f40558bc88c9f4dae28207ed5e415
Sam Sanoop <sams@snyk.io>
2022-02-06 15:51:59 +0000

    Create express.js

---
+++ SSRF/express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router()
+const request = require('request');
+
+router.post('/downlad-url', (req, res) => {
+    downloadURL(req.body.url, () =>{
+        res.send('Done')
+    }) 
+});
+
+const downloadURL = (url, onend) => {
+    const opts = {
+      uri: url,
+      method: 'GET',
+      followAllRedirects: true
+    }
+  
+    request(opts)
+      .on('data', ()=>{})
+      .on('end', () => onend())
+      .on('error', (err) => console.log(err, 'controller.url.download.error'))
+}
+
+module.exports = router
