df24a73bdc747391253f10c6d309b5797426dcbf
Poornima P <44209006+poornimadevip@users.noreply.github.com>
2023-04-07 20:59:08 +0530

    add new vuln code

---
+++ java/SI.java
@@ -0,0 +1,79 @@
+package org.joychou.controller;
+
+
+import org.joychou.mapper.UserMapper;
+import org.joychou.dao.User;
+import org.joychou.security.SecurityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.sql.*;
+import java.util.List;
+
+
+/**
+ * SQL Injection
+ *
+ * @author JoyChou @2018.08.22
+ */
+
+@SuppressWarnings("Duplicates")
+@RestController
+@RequestMapping("/sqli")
+public class SQLI {
+
+    private static final Logger logger = LoggerFactory.getLogger(SQLI.class);
+  
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Resource
+    private UserMapper userMapper;
+
+    @RequestMapping("/jdbc/vuln")
+    public String jdbc_sqli_vul(@RequestParam("username") String username) {
+
+        StringBuilder result = new StringBuilder();
+
+        try {
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(url, user, password);
+
+            if (!con.isClosed())
+                System.out.println("Connect to database successfully.");
+
+            Statement statement = con.createStatement();
+            String sql = "select * from users where username = '" + username + "'";
+            logger.info(sql);
+            ResultSet rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                String res_name = rs.getString("username");
+                String res_pwd = rs.getString("password");
+                String info = String.format("%s: %s\n", res_name, res_pwd);
+                result.append(info);
+                logger.info(info);
+            }
+            rs.close();
+            con.close();
+
+
+        } catch (ClassNotFoundException e) {
+            logger.error("Sorry, can't find the Driver!");
+        } catch (SQLException e) {
+            logger.error(e.toString());
+        }
+        return result.toString();
+    }
+}
