809946710359e6de0f50bf316457717ffb784210
Poornima P <44209006+poornimadevip@users.noreply.github.com>
2023-04-07 21:16:14 +0530

    Create XSSRemoved.java

---
+++ java/XSSRemoved.java
@@ -0,0 +1,35 @@
+package org.controller;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/xss")
+public class XSS {
+
+    /**
+     * safe Code.
+     * http://localhost:8080/xss/safe
+     */
+    @RequestMapping("/safe")
+    @ResponseBody
+    public static String safe(String xss) {
+        return encode(xss);
+    }
+
+    private static String encode(String origin) {
+        origin = StringUtils.replace(origin, "&", "&amp;");
+        origin = StringUtils.replace(origin, "<", "&lt;");
+        origin = StringUtils.replace(origin, ">", "&gt;");
+        origin = StringUtils.replace(origin, "\"", "&quot;");
+        origin = StringUtils.replace(origin, "'", "&#x27;");
+        origin = StringUtils.replace(origin, "/", "&#x2F;");
+        return origin;
+    }
+}
