e68a6c7e664318014a62385e94782ff729272dd9
Poornima P <44209006+poornimadevip@users.noreply.github.com>
2023-04-07 21:15:34 +0530

    Create XSS.java

---
+++ java/XSS.java
@@ -0,0 +1,21 @@
+package org.joychou.controller;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/xss")
+public class XSS {
+
+    @RequestMapping("/reflect")
+    @ResponseBody
+    public static String reflect(String xss) {
+        return xss;
+    }
+}
