7d2f6e10cc0612e8d4bb47a87187a282e6335578
0xf00I <114118401+0xf00I@users.noreply.github.com>
2023-04-18 19:06:59 +0000

    Update strcpy.c

--- Buffer Overflow/strcpy.c
+++ Buffer Overflow/strcpy.c
@@ -1,3 +1,19 @@
-char str1[10];
-char str2[]="abcdefghijklmn";
-strcpy(str1,str2);
+void overflow_function (char *str)
+{
+  char buffer[20];
+
+  strcpy(buffer, str);  // Function that copies str to buffer
+}
+
+int main()
+{
+  char big_string[128];
+  int i;
+
+  for(i=0; i < 128; i++)  // Loop 128 times
+  {
+    big_string[i] = 'A'; // And fill big_string with 'A's
+  }
+  overflow_function(big_string);
+  exit(0);
+}
